50aacbccaddeeb1941f0703b8fcfdb954b858a8c
Jacob de Vries <mrjacobdevries@gmail.com>
2021-01-06 18:28:57 -0800

    Updating. Some experimenting on pypoll. Issues still with trying not to iterate through too many times. Going to try messy solve and then clean up later.

--- pypoll/main.py
+++ pypoll/main.py
@@ -21,20 +21,25 @@ with open(election_csv) as csv_file:
     candidate_list = []
 
     for row in csv_reader:
+        candidate = str(row[2])
+        for row in csv_reader:
+            if row[2] == candidate:
+                candidate_votes = candidate_votes + int(row[0])
+                print(row)
         total_votes = total_votes + int(row[0])
-        candidate_list.append(str(row[2]))
+        
 
-    candidate_list = set(candidate_list)
-    number_of_candidates = len(candidate_list)
 
-    for row in csv_reader:
-        candidate_list[candidate]
+
+    # for row 2, grab the candidate name and add it to the dict as a key. grab the votes as its value. 
+    # then loop through and check for each candidate name if it already exists as a key.
+    # if yes, grab and add the new votes. if no, grab the new candidate and votes. repeat.
 
 # console output
-print(f"\nElection Results \n------------------- \nTotal Votes: {total_votes:,} \n------------------- \n")
+print(f"\nElection Results \n------------------- \nTotal Votes: {total_votes:,} \n------------------- \n------------------- \nWinner: {election_winner} \n------------------- \n")
 
 # sends analysis information to a text file in the analysis file
 # overwrites if there's a duplicate file already there
 budget_data_analysis = open("analysis/election_data_analysis.txt", "w")
-budget_data_analysis.write(f"\nElection Results \n------------------- \nTotal Votes: {total_votes:,} \n------------------- \n")
+budget_data_analysis.write(f"\nElection Results \n------------------- \nTotal Votes: {total_votes:,} \n------------------- \n------------------- \nWinner: {election_winner} \n------------------- \n")
 budget_data_analysis.close()
